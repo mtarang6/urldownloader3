aaa5282eb3199e02b2d37e1781dc2f4cd938c9ae
Vicky Mishra <vm1129371@gmail.com>
2021-08-19 19:41:49 +0530

    cv

--- app/src/main/java/com/tara/basicregistrartion/ApiCallingActivity.java
+++ app/src/main/java/com/tara/basicregistrartion/ApiCallingActivity.java
@@ -1,5 +1,9 @@
 package com.tara.basicregistrartion;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -8,6 +12,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -23,6 +28,7 @@ public class ApiCallingActivity extends AppCompatActivity {
     AdapterResponse adapterResponse;
     AppCompatEditText et_id;
     private Button btn_submit;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,60 +36,81 @@ public class ApiCallingActivity extends AppCompatActivity {
         recyclerview = (RecyclerView)findViewById(R.id.recyclerview);
         et_id = (AppCompatEditText)findViewById(R.id.et_id);
         btn_submit = (Button)findViewById(R.id.btn_submit);
-        RetroInterface service = ApiClient.getClient().create(RetroInterface.class);
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String number = et_id.getText().toString();
-                int num = Integer.parseInt(number);
-                Log.d("Tarang", "onClick: "+num);
-                if(num != 0){
-                    Call<List<Response>> listCalls = service.getResponseListWithId(num);
-                    listCalls.enqueue(new Callback<List<Response>>() {
-                        @Override
-                        public void onResponse(Call<List<Response>> call, retrofit2.Response<List<Response>> response) {
-                            if(response.isSuccessful()){
-                                List<Response> responseList = response.body();
-                                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ApiCallingActivity.this);
-                                adapterResponse = new AdapterResponse(ApiCallingActivity.this,responseList);
-                                recyclerview.setLayoutManager(linearLayoutManager);
-                                recyclerview.setAdapter(adapterResponse);
-                                adapterResponse.notifyDataSetChanged();
-                                Toast.makeText(ApiCallingActivity.this, "success", Toast.LENGTH_SHORT).show();
+
+        if(isConnected()){
+            RetroInterface service = ApiClient.getClient().create(RetroInterface.class);
+            btn_submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String number = et_id.getText().toString();
+                    int num = Integer.parseInt(number);
+                    Log.d("Tarang", "onClick: "+num);
+                    if(num != 0){
+                        Call<List<Response>> listCalls = service.getResponseListWithId(num);
+                        listCalls.enqueue(new Callback<List<Response>>() {
+                            @Override
+                            public void onResponse(Call<List<Response>> call, retrofit2.Response<List<Response>> response) {
+                                if(response.isSuccessful()){
+                                    List<Response> responseList = response.body();
+                                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ApiCallingActivity.this);
+                                    adapterResponse = new AdapterResponse(ApiCallingActivity.this,responseList);
+                                    recyclerview.setLayoutManager(linearLayoutManager);
+                                    recyclerview.setAdapter(adapterResponse);
+                                    adapterResponse.notifyDataSetChanged();
+                                    Toast.makeText(ApiCallingActivity.this, "success", Toast.LENGTH_SHORT).show();
+                                    et_id.setText("");
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onFailure(Call<List<Response>> call, Throwable t) {
-                            Toast.makeText(ApiCallingActivity.this, "failed", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }else{
-                    Toast.makeText(ApiCallingActivity.this, "Enter Number", Toast.LENGTH_SHORT).show();
+                            @Override
+                            public void onFailure(Call<List<Response>> call, Throwable t) {
+                                Toast.makeText(ApiCallingActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }else{
+                        Toast.makeText(ApiCallingActivity.this, "Enter Number", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
+            });
 
-       Call<List<Response>> listCall = service.getResponseList();
-       listCall.enqueue(new Callback<List<Response>>() {
-           @Override
-           public void onResponse(Call<List<Response>> call, retrofit2.Response<List<Response>> response) {
-               if(response.isSuccessful()){
-                   List<Response> responseList = response.body();
-                   LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ApiCallingActivity.this);
-                   adapterResponse = new AdapterResponse(ApiCallingActivity.this,responseList);
-                   recyclerview.setLayoutManager(linearLayoutManager);
-                   recyclerview.setAdapter(adapterResponse);
-                   adapterResponse.notifyDataSetChanged();
-                   Toast.makeText(ApiCallingActivity.this, "success", Toast.LENGTH_SHORT).show();
-               }
-           }
+            Call<List<Response>> listCall = service.getResponseList();
+            listCall.enqueue(new Callback<List<Response>>() {
+                @Override
+                public void onResponse(Call<List<Response>> call, retrofit2.Response<List<Response>> response) {
+                    if(response.isSuccessful()){
+                        List<Response> responseList = response.body();
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ApiCallingActivity.this);
+                        adapterResponse = new AdapterResponse(ApiCallingActivity.this,responseList);
+                        recyclerview.setLayoutManager(linearLayoutManager);
+                        recyclerview.setAdapter(adapterResponse);
+                        adapterResponse.notifyDataSetChanged();
+                        Toast.makeText(ApiCallingActivity.this, "success", Toast.LENGTH_SHORT).show();
+                    }
+                }
 
-           @Override
-           public void onFailure(Call<List<Response>> call, Throwable t) {
-               Toast.makeText(ApiCallingActivity.this, "failed", Toast.LENGTH_SHORT).show();
-           }
-       });
+                @Override
+                public void onFailure(Call<List<Response>> call, Throwable t) {
+                    Toast.makeText(ApiCallingActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                }
+            });
 
+        }else{
+            Toast.makeText(this, "Connect The Internet", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public boolean isConnected() {
+        boolean connected = false;
+        try {
+            ConnectivityManager cm = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo nInfo = cm.getActiveNetworkInfo();
+            connected = nInfo != null && nInfo.isAvailable() && nInfo.isConnected();
+            return connected;
+        } catch (Exception e) {
+            Log.e("Connectivity Exception", e.getMessage());
+        }
+        return connected;
     }
 }
